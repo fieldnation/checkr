64f2ad77f78c46479969e3600f0a5a22f77bb6fa
jackspirou <jack.spirou@fieldnation.com>
2016-06-21 14:35:09 -0500

    add SetAPIKey method

--- checkr.go
+++ checkr.go
@@ -1,5 +1,12 @@
 package checkr
 
+// v1 is the checkr v1 api URL
 const v1 = "https://api.checkr.com/v1/"
 
+// apiKey is the key to use to authenticate and authorize HTTP requests
 var apiKey string
+
+// SetAPIKey sets an API key.
+func SetAPIKey(key string) {
+	apiKey = key
+}
